baa299b7f553828a94ee6856b857073ae83fb249
pactera-479 <707944442@qq.com>
2019-11-23 15:22:46 +0800

    新增项目

--- eureka-zuul/src/main/java/com/laivie/filter/SelfDefineFilter.java
+++ eureka-zuul/src/main/java/com/laivie/filter/SelfDefineFilter.java
@@ -1,36 +1,61 @@
 package com.laivie.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 public class SelfDefineFilter extends ZuulFilter {
     @Override
     public String filterType() {
         //过滤器执行阶段的类型
-        System.out.println("过滤器执行阶段的类型");
+        System.out.println("过滤器执行阶段的类型"+FilterConstants.ROUTE_TYPE);
+        /*
+        pre：可以在请求被路由之前调用
+        route：在路由请求时候被调用
+        post：在route和error过滤器之后被调用
+        error：处理请求时发生错误时被调用
+         */
         return FilterConstants.ROUTE_TYPE;
     }
 
     @Override
     public int filterOrder() {
         //执行顺序
-        System.out.println("执行顺序");
-        return 0;
+        System.out.println("执行顺序"+0);
+        return 0;// 优先级为0，数字越大，优先级越低
     }
 
     @Override
     public boolean shouldFilter() {
         //是否执行当前过滤器
-        System.out.println("是否执行当前过滤器");
+        System.out.println("是否执行当前过滤器"+true);
 
-        return false;
+        return false;// 是否执行该过滤器，此处为true，说明需要过滤
     }
 
     @Override
     public Object run() throws ZuulException {
         System.out.println("执行自定义过滤器");
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        System.out.println(String.format("%s AccessUserNameFilter request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        String username = request.getParameter("username");// 获取请求的参数
+        if(null != username && username.equals("chhliu")) {// 如果请求的参数不为空，且值为chhliu时，则通过
+            ctx.setSendZuulResponse(true);// 对该请求进行路由
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);// 设值，让下一个Filter看到上一个Filter的状态
+        }else{
+            ctx.setSendZuulResponse(false);// 过滤该请求，不对其进行路由
+            ctx.setResponseStatusCode(401);// 返回错误码
+            ctx.setResponseBody("{\"result\":\"username is not correct!\"}");// 返回错误内容
+            ctx.set("isSuccess", false);
+        }
         return null;
     }
 }
